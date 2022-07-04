91592be112991fe936f1a1bb2e722525aa383bfd
Aaron Guzman <aarongzmn@gmail.com>
2022-07-03 23:14:04 -0700

    formatting

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from chalice import Chalice
 import requests
 import boto3
 
-app = Chalice(app_name='helloworld')
+app = Chalice(app_name="helloworld")
 
 
-@app.route('/')
+@app.route("/")
 def index():
     try:
         r = requests.get("http://worldtimeapi.org/api/timezone/America/Los_Angeles")
@@ -18,7 +18,7 @@ def index():
         return {"error": e.text}
 
 
-@app.route('/guess_my_name/{name}')
+@app.route("/guess_my_name/{name}")
 def guess_secret(name):
     if name:
         secret_name = "test/my_name"
@@ -27,30 +27,23 @@ def guess_secret(name):
         secret_json = json.loads(secret_value)
         secret_fullname = secret_json["first_name"] + " " + secret_json["last_name"]
         if secret_fullname == unquote(name):
-            return {
-                "Guess Status": "Correct"
-                }
+            return {"Guess Status": "Correct"}
         else:
             return {
                 "Guess Status": "Incorrect",
                 "Your Guess": name,
-                "Correct Answer": secret_fullname
-                }
+                "Correct Answer": secret_fullname,
+            }
     else:
         return {"error": "Did you forget to guess a name?"}
 
 
 def get_secret_value(secret_name, region_name):
     session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
+    client = session.client(service_name="secretsmanager", region_name=region_name)
 
-    get_secret_value_response = client.get_secret_value(
-        SecretId=secret_name
-    )
-    secret_value = get_secret_value_response['SecretString']
+    get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    secret_value = get_secret_value_response["SecretString"]
     return secret_value
 
 
